d3a202ecc43af9428ad2d054c2b48bdd8e3e70ba
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2023-02-20 18:36:52 -0300

    CHANGE: use channel to communicate between threads
    Each thread passes the min, max values to the main thread
    The main thread prints and updates the global min, max values
    Once all transmitters are closed the main thread prints the results
    No significant performance changes were noted

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
 use std::borrow::Borrow;
-use std::io::{BufReader, BufRead};
+use std::io::{BufRead, BufReader};
+use std::sync::mpsc::{channel, Sender};
 use std::thread::{available_parallelism, JoinHandle};
-
 
 // Passagem de argumentos utilizando a crate clap
 #[derive(Parser, Debug)]
@@ -11,7 +11,7 @@ struct Args {
     /// Caminho do arquivo com matriz de adjacência no formato .csv separado por ';'
     #[arg(short, long)]
     caminho_arquivo_grafo: String,
-    
+
     /// Número de threads para utilizar no programa (0 todas)
     #[arg(short, long, default_value_t = 0)]
     numero_threads: usize,
@@ -24,13 +24,16 @@ fn main() {
     // Lê o grafo
     let grafo = ler_arquivo(args.caminho_arquivo_grafo.borrow());
     let grafo = Box::new(grafo);
-    
-    let numero_threads =  {
+
+    let numero_threads = {
         let total_threads = available_parallelism().unwrap().get();
-        if  total_threads < args.numero_threads || args.numero_threads == 0 { total_threads }
-        else { args.numero_threads }
+        if total_threads < args.numero_threads || args.numero_threads == 0 {
+            total_threads
+        } else {
+            args.numero_threads
+        }
     };
-    
+
     // Variáveis com os resultados finais
     let mut minimo = u32::MAX;
     let mut indice_minimo = 0;
@@ -41,34 +44,44 @@ fn main() {
     let antes = std::time::Instant::now();
 
     // Multithread stuff
-    let mut thread_handler: Vec<JoinHandle<((u32, u64), (u32, u64))>> = Vec::new();
     println!("Utilizando {numero_threads} threads");
     let numero_permutacoes_por_thread = calcula_fatorial(grafo.len() - 1) / numero_threads;
-    
+    let (tx, rx) = channel::<(u32, u64)>();
+
     // Criacao das threads
     for i in 0..numero_threads {
         let indice_primeira_permutacao = i * numero_permutacoes_por_thread;
         let indice_ultima_permutacao = (i + 1) * numero_permutacoes_por_thread;
         let grafo = grafo.clone();
-        let thread = std::thread::spawn(move || {
-            tsp_forca_bruta(grafo, indice_primeira_permutacao as u64, indice_ultima_permutacao as u64)
+        let transmitter_end = tx.clone();
+        std::thread::spawn(move || {
+            tsp_forca_bruta(
+                grafo,
+                indice_primeira_permutacao as u64,
+                indice_ultima_permutacao as u64,
+                transmitter_end,
+            );
         });
-        thread_handler.push(thread);
     }
-    
-    // Aguardando todas as threads terminarem 
-    for thread in thread_handler {
-        let a = thread.join().unwrap();
-        if a.0.0 < minimo {
-            (minimo, indice_minimo) = a.0;
-        }
-        if a.1.0 > maximo {
-            (maximo, indice_maximo) = a.1;
+
+    drop(tx);
+
+    while let Ok(valor) = rx.recv() {
+        if valor.0 < minimo {
+            minimo = valor.0;
+            indice_minimo = valor.1;
+            print!("\rMenor valor {minimo}\tMaior valor {maximo}");
+        } else if valor.0 > maximo {
+            maximo = valor.0;
+            indice_maximo = valor.1;
+            print!("\rMenor valor {minimo}\tMaior valor {maximo}");
         }
     }
+
     let agora = std::time::Instant::now();
-    
+
     // Mostra o peso e o caminho
+    println!();
     println!("\tMenor caminho: {minimo}");
     imprime_ordem_lexicografica_n(indice_minimo, grafo.len());
     println!("\tMaior caminho: {maximo}");
@@ -83,33 +96,32 @@ fn main() {
     println!("Numero de threads utilizadas: {numero_threads}")
 }
 
-fn imprime_grafo(grafo : &Vec<Vec<u32>>){
+fn imprime_grafo(grafo: &Vec<Vec<u32>>) {
     for item in grafo {
         println!("{:?}", item);
     }
 }
 
-fn imprime_ordem_lexicografica_n(n : u64, numero_elementos : usize){
-    let mut vetor:Vec<usize> = (0..numero_elementos).collect();
-    
+fn imprime_ordem_lexicografica_n(n: u64, numero_elementos: usize) {
+    let mut vetor: Vec<usize> = (0..numero_elementos).collect();
+
     n_esima_ordem_lexicografica(&mut vetor, n);
 
     println!("{:?}", vetor);
 }
 
-fn ler_arquivo(caminho : &str) -> Vec<Vec<u32>>{
-    let arquivo = std::fs::File::open(caminho)
-                                        .expect("Não foi possível abrir o arquivo\n");
-    
+fn ler_arquivo(caminho: &str) -> Vec<Vec<u32>> {
+    let arquivo = std::fs::File::open(caminho).expect("Não foi possível abrir o arquivo\n");
+
     let mut buf_reader = BufReader::new(arquivo);
     let mut linha = String::new();
-    
-    let mut grafo:Vec<Vec<u32>> = Vec::new();
+
+    let mut grafo: Vec<Vec<u32>> = Vec::new();
 
     while buf_reader.read_line(&mut linha).unwrap() > 0 {
         // Remove o \n do final da linha, para não ocorrer erro com o split
         if linha.ends_with("\n") {
-           linha.pop();
+            linha.pop();
         }
 
         // Adiciona uma nova linha no grafo
@@ -129,27 +141,37 @@ fn ler_arquivo(caminho : &str) -> Vec<Vec<u32>>{
     return grafo;
 }
 
-fn proxima_ordem_lexicografica(vetor: &mut Vec<usize>) -> bool{
-    
-    if vetor.len() <= 1 {return  false;}
+fn proxima_ordem_lexicografica(vetor: &mut Vec<usize>) -> bool {
+    if vetor.len() <= 1 {
+        return false;
+    }
 
     let mut i = vetor.len() - 2;
     let mut j = vetor.len() - 1;
 
-    while (i > 0) && (vetor[i] > vetor[i + 1]){i-=1;};
-    while (j > 0) && (vetor[j] < vetor[i]) {j-=1;};
+    while (i > 0) && (vetor[i] > vetor[i + 1]) {
+        i -= 1;
+    }
+    while (j > 0) && (vetor[j] < vetor[i]) {
+        j -= 1;
+    }
 
     if i != j {
         vetor.swap(i, j);
-        let pedaco_inverter = &mut vetor[i+1..];
+        let pedaco_inverter = &mut vetor[i + 1..];
         pedaco_inverter.reverse();
         return true;
     }
 
-    return  false;
+    return false;
 }
 
-fn tsp_forca_bruta(grafo : Box<Vec<Vec<u32>>>, indice_primeira_permutacao :u64, indice_ultima_permutacao : u64) -> ((u32, u64), (u32, u64)){
+fn tsp_forca_bruta(
+    grafo: Box<Vec<Vec<u32>>>,
+    indice_primeira_permutacao: u64,
+    indice_ultima_permutacao: u64,
+    transmitter: Sender<(u32, u64)>,
+) -> ((u32, u64), (u32, u64)) {
     let mut min: u32 = std::u32::MAX;
     let mut indice_min: u64 = 0;
     let mut max: u32 = std::u32::MIN;
@@ -157,7 +179,7 @@ fn tsp_forca_bruta(grafo : Box<Vec<Vec<u32>>>, indice_primeira_permutacao :u64, 
     let mut peso_caminho_atual: u32;
 
     let mut permutacao_atual = indice_primeira_permutacao;
-    
+
     // Cria o vetor com a permutação atual, contém o índice do vértice
     // na matriz
     let mut caminho_atual: Vec<usize> = (0..grafo.len()).collect();
@@ -168,61 +190,66 @@ fn tsp_forca_bruta(grafo : Box<Vec<Vec<u32>>>, indice_primeira_permutacao :u64, 
     // Calcular o peso e atualiza a mínima e máxima
     while permutacao_atual < indice_ultima_permutacao {
         peso_caminho_atual = 0;
-        for indice in 0..caminho_atual.len() {
-            peso_caminho_atual += grafo[caminho_atual[indice]][caminho_atual[(indice + 1) % caminho_atual.len()]];
-        }
+        (0..caminho_atual.len()).for_each(|indice| {
+            peso_caminho_atual +=
+                grafo[caminho_atual[indice]][caminho_atual[(indice + 1) % caminho_atual.len()]];
+        });
 
         if peso_caminho_atual < min {
             indice_min = permutacao_atual;
             min = peso_caminho_atual;
-            println!("Menor PESO: {min}");
+            transmitter.send((min, indice_min)).unwrap();
         }
-        
+
         if peso_caminho_atual > max {
             indice_max = permutacao_atual;
             max = peso_caminho_atual;
-            println!("Maior PESO: {max}");
+            transmitter.send((max, indice_max)).unwrap();
         }
 
         permutacao_atual += 1;
-        
-        if proxima_ordem_lexicografica(&mut caminho_atual) == false {break;}
+
+        if proxima_ordem_lexicografica(&mut caminho_atual) == false {
+            break;
+        }
     }
 
     return ((min, indice_min), (max, indice_max));
 }
 
-
-fn n_esima_ordem_lexicografica(vetor: &mut Vec<usize>, n: u64){
+fn n_esima_ordem_lexicografica(vetor: &mut Vec<usize>, n: u64) {
     // https://ichi.pro/pt/permutacoes-eficientes-em-ordem-lexicografica-156131986289848
 
-    if vetor.len() < 1 {return;}
+    if vetor.len() < 1 {
+        return;
+    }
 
     // Calcula fatorial de (numero de itens - 1)
-    let mut fatorial : usize = calcula_fatorial(vetor.len() - 1);
+    let mut fatorial: usize = calcula_fatorial(vetor.len() - 1);
 
     // Verifica se a posição lexicográfica existe para o tamanho do vetor
-    if n >= (fatorial * vetor.len()).try_into().unwrap() {return;}
+    if n >= (fatorial * vetor.len()).try_into().unwrap() {
+        return;
+    }
 
-    let mut resto_anterior : usize = n.try_into().unwrap();
-    let mut indice_proximo : usize;
+    let mut resto_anterior: usize = n.try_into().unwrap();
+    let mut indice_proximo: usize;
 
     // Cria uma cópia do vetor original, deixa ordenada (1ª ordem lexicográfica)
     // e limpa o vetor original
-    let mut indices:Vec<usize> = vetor.clone();
+    let mut indices: Vec<usize> = vetor.clone();
     indices.sort();
     vetor.clear();
-    
+
     while (indices.len() > 0) && fatorial > 0 {
-        // Calcula o índice do próximo item 
+        // Calcula o índice do próximo item
         indice_proximo = resto_anterior / fatorial;
         resto_anterior = resto_anterior % fatorial;
-        
+
         // Aqui é calculado o fatorial de indices.len() - 1
         if indices.len() - 1 > 0 {
             fatorial = fatorial / (indices.len() - 1);
-        }
-        else {
+        } else {
             fatorial = 1;
         }
 
@@ -232,8 +259,8 @@ fn n_esima_ordem_lexicografica(vetor: &mut Vec<usize>, n: u64){
     return;
 }
 
-fn calcula_fatorial(numero : usize) -> usize {
-    let mut resultado : usize = 1;
+fn calcula_fatorial(numero: usize) -> usize {
+    let mut resultado: usize = 1;
     let mut numero = numero;
     if numero > 1 {
         while numero > 0 {
@@ -241,5 +268,5 @@ fn calcula_fatorial(numero : usize) -> usize {
             numero -= 1;
         }
     }
-    return  resultado;
+    return resultado;
 }
